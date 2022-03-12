28c7263493f7637ac60fd89c17d8976e64b8c2dd
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:55 -0300

    chore: re index nimfasttext

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''fastText wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimfastText-master.flake = false;
+  inputs.src-nimfastText-master.owner = "genotrance";
+  inputs.src-nimfastText-master.ref   = "refs/heads/master";
+  inputs.src-nimfastText-master.repo  = "nimfastText";
+  inputs.src-nimfastText-master.type  = "github";
+  
+  inputs."nimgen".dir   = "nimpkgs/n/nimgen";
+  inputs."nimgen".owner = "riinr";
+  inputs."nimgen".ref   = "flake-pinning";
+  inputs."nimgen".repo  = "flake-nimble";
+  inputs."nimgen".type  = "github";
+  inputs."nimgen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimfastText-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimfastText-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
